f60a0484a3d1d4cb516c7ee82baff4e511bcb779
AlejandroC <alexcabero@hotmail.com>
2024-05-18 03:34:02 -0500

    Pagina de producto hecha

--- src/Componentes/Productos.jsx
+++ src/Componentes/Productos.jsx
@@ -5,6 +5,8 @@ import SearchBox from './SearchBox'; // Importa el componente SearchBox
 
 function Productos() {
   const [filteredProducts, setFilteredProducts] = useState([]); // Aquí debes usar tu lista de productos
+  const itemsPerPage = 10; // Cambia esto según tus necesidades
+  const [currentPage, setCurrentPage] = useState(1);
 
   const handleSearch = (query) => {
     // Filtra los productos según la consulta de búsqueda (query)
@@ -18,7 +20,13 @@ function Productos() {
       );
     });
     setFilteredProducts(filtered);
+    setCurrentPage(1); // Reinicia la página actual al realizar una búsqueda
   };
+
+  // Calcula los índices para la paginación
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = filteredProducts.slice(indexOfFirstItem, indexOfLastItem);
 
   return (
     <div className="productos-container">
@@ -45,7 +53,7 @@ function Productos() {
           </tr>
         </thead>
         <tbody>
-          {filteredProducts.map((product) => (
+          {currentItems.map((product) => (
             <tr key={product.id}>
               <td>{product.id}</td>
               <td>{product.detalle}</td>
@@ -59,6 +67,16 @@ function Productos() {
           ))}
         </tbody>
       </table>
+      {/* Componente de navegación de paginación */}
+      <div className="pagination">
+        <button onClick={() => setCurrentPage(currentPage - 1)} disabled={currentPage === 1}>
+          Anterior
+        </button>
+        <span> {currentPage} </span>
+        <button onClick={() => setCurrentPage(currentPage + 1)} disabled={currentItems.length < itemsPerPage}>
+          Siguiente
+        </button>
+      </div>
     </div>
   );
 }
