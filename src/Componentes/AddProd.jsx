ab7ec472680f6ae8b7a5942cd0c5ce28370419e6
AlejandroC <alexcabero@hotmail.com>
2024-05-18 17:54:58 -0500

    Finalizado con Agregar Producto / Ver detalle de Producto

--- src/Componentes/AddProd.jsx
+++ src/Componentes/AddProd.jsx
@@ -32,62 +32,91 @@ function AddProd() {
   const handleImagenChange = (e) => {
     // Maneja el cambio de la imagen
     const file = e.target.files[0];
-    setImagen(file);
+    setImagen(URL.createObjectURL(file));
   };
 
   return (
     <div className="Agregacion">
       <div className="Cabezon">Agregar Producto</div>
       <div className="Formulario">
-      <div className="ImagenContainer"> 
+      <div className="ImagenContainer">
+      <div className='ImageLoad'>
+      {imagen && <img src={imagen} alt="Imagen seleccionada" />}
+      </div>
+      <div className='peque'> 
       <input
           type="file"
           accept="image/*"
           onChange={handleImagenChange} // Maneja el cambio de la imagen
         />
-      </div>
+        </div>
+      </div>     
       <div className="Forms">
+        <label htmlFor="nombre">Nombre:</label>
+        <br />
         <input
           type="text"
           placeholder="Nombre"
           value={nombre}
           onChange={(e) => setNombre(e.target.value)}
         />
+        <br />
+        <label htmlFor="descripcion">Descripcion:</label>
+        <br />
         <textarea
           placeholder="Descripción"
           value={descripcion}
           onChange={(e) => setDescripcion(e.target.value)}
         />
+        <br />
+        <label htmlFor="caracteristicas">Carateriscas:</label>
+        <br />
         <input
           type="text"
           placeholder="Características"
           value={caracteristicas}
           onChange={(e) => setCaracteristicas(e.target.value)}
         />
+        <br />
+        <label htmlFor="marca">Marca:</label>
+        <br />
         <input
           type="text"
           placeholder="Marca"
           value={marca}
           onChange={(e) => setMarca(e.target.value)}
         />
+        <br />
+        <label htmlFor="serie">Serie:</label>
+        <br />
         <input
           type="text"
           placeholder="Serie"
           value={serie}
           onChange={(e) => setSerie(e.target.value)}
         />
+        <br />
+        <label htmlFor="precio">Precio:</label>
+        <br />
         <input
           type="text"
           placeholder="Precio"
           value={precio}
           onChange={(e) => setPrecio(e.target.value)}
         />
+        <br />
+        <label htmlFor="tipo">Tipo:</label>
+        <br />
         <input
           type="text"
           placeholder="Tipo"
           value={tipo}
           onChange={(e) => setTipo(e.target.value)}
         />
+        <br />
+        <label htmlFor="descripcion">Descripcion:</label>
+        <br />
+        <br />
         <input
           type="text"
           placeholder="Stock"
